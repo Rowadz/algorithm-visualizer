4a3929350a481f8f2c227151e695943a19271e8c
MohammedAl-rowad <rowadwo@gmail.com>
2021-06-12 22:00:23 +0300

    Fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,8 @@ const App: FC = () => {
   return (
     <ChakraProvider theme={theme}>
       <AppHeader />
-      <Flex height="100vh" flexDir={['column', 'row', 'row', 'row']}>
-        <Box
-          h="100%"
-          w="25%"
-          display={['none', 'none', 'inherit', 'inherit']}
-          p=".5rem"
-        >
+      <Flex height="100vh" flexDir={['column', 'column', 'row', 'row']}>
+        <Box h="100%" w={['100%', '100%', '25%', '25%']} p=".5rem">
           <AlgorithmSelector />
         </Box>
         <Box h="100%" w={['100%', '100%', '75%', '75%']}>
