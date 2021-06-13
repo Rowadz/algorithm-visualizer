2ea6701f7f82ae6812c9ccf5daaf0170aa2585a6
MohammedAl-rowad <rowadwo@gmail.com>
2021-06-14 00:17:35 +0300

    Testing the tree lib #4

--- src/containers/AlgorithmViz/VizComponent/MergeSortViz/MergeSortViz.tsx
+++ src/containers/AlgorithmViz/VizComponent/MergeSortViz/MergeSortViz.tsx
@@ -1,26 +1,7 @@
+import { memo, useState } from 'react'
 import { useCustomColors } from 'app/hooks'
-import { memo } from 'react'
 import Tree from 'react-d3-tree'
 import styles from './MergeSortViz.module.css'
-
-const orgChart = {
-  name: '[1, 2 ,3, 5]',
-  children: [
-    {
-      name: '[1, 2]',
-      children: [
-        {
-          name: '[3,5]',
-          children: [{ name: '[1,2,3,5]' }],
-        },
-        {
-          name: '[1,2,3,5]',
-          children: [{ name: '[1,2,3,5]' }],
-        },
-      ],
-    },
-  ],
-}
 
 const renderForeignObjectNode = ({
   nodeDatum,
@@ -40,14 +21,40 @@ const renderForeignObjectNode = ({
 )
 
 const MergeSortViz = () => {
+  const [tree, setTree] = useState({
+    name: '[1, 2 ,3, 5]',
+    children: [
+      {
+        name: '[1, 2]',
+        children: [
+          {
+            name: '[3,5]',
+            children: [{ name: '[1,2,3,5]' }],
+          },
+          {
+            name: '[1,2,3,5]',
+            children: [{ name: '[1,2,3,5]' }],
+          },
+        ],
+      },
+    ],
+  })
+
+  const onClick = () => {
+    setTree({
+      ...tree,
+      children: [...tree.children, { name: '1', children: [] }],
+    })
+  }
   const { saltBox } = useCustomColors()
   const foreignObjectProps = { width: 200, height: 200, x: 20 }
 
   return (
     <div style={{ width: '50rem', height: '100vh' }}>
+      <button onClick={onClick}>test</button>
       <Tree
         translate={{ x: window.innerWidth / 4, y: 20 }}
-        data={orgChart}
+        data={tree}
         renderCustomNodeElement={(rd3tProps) =>
           renderForeignObjectNode({
             ...rd3tProps,
