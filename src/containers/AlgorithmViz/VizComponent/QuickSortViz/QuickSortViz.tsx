d1ed8a2b36d9a26acbb1fa859536c733a5a0f157
MohammedAl-rowad <rowadwo@gmail.com>
2021-08-27 19:34:39 +0300

    Start working on quick sort

---
+++ src/containers/AlgorithmViz/VizComponent/QuickSortViz/QuickSortViz.tsx
@@ -0,0 +1,17 @@
+import { memo, FC } from 'react'
+import { Flex, Button } from '@chakra-ui/react'
+
+const QuickSortViz: FC = () => {
+  return (
+    <>
+      <Flex>
+        <Button m="1rem">START!</Button>
+      </Flex>
+      <Flex fontWeight="bold" flexWrap="wrap">
+        TODO
+      </Flex>
+    </>
+  )
+}
+
+export default memo(QuickSortViz)
---
+++ src/containers/AlgorithmViz/VizComponent/QuickSortViz/index.ts
@@ -0,0 +1,1 @@
+export { default as QuickSortViz } from './QuickSortViz'
